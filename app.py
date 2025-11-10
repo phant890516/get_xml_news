da8c2c068e21efb0860aac4fad9a24f42b650dd9
phant <phant890516@gmail.com>
2025-11-10 12:50:40 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+from flask_cors import CORS
+import requests
+import os
+app = Flask("__main__")
+CORS(app)
+
+#処理
+@app.route("/")
+def hello():
+    return render_template(
+        'index.html',
+        title='XMLでデータ処理',
+        message='Built for Javascript and Python'
+    )
+# 非同期処理
+@app.route('/get_rss_xml', methods=['GET'])
+def get_external_xml():
+    # script.jsから送信されたデータを取得
+    selectVal = request.args.get('val')
+    xml_file_path = os.path.join('static', selectVal)
+    if not os.path.exists(xml_file_path):
+        return "File not found", 404
+    # XML ファイルを読み込む
+    with open(xml_file_path, 'r', encoding='utf-8') as f:
+        xml_content = f.read()
+    
+    #urlの場合    
+    # selectVal = request.args.get('val')
+    # xml_url = selectVal
+    # response = requests.get(xml_url)
+    # return response.text, {'Content-Type': 'application/xml; charset=utf-8'}
+
+    return xml_content, {'Content-Type': 'application/xml; charset=utf-8'}
+
+# アプリケーションの実行
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='127.0.0.1',port='1050')
---
+++ static/js/script.js
@@ -0,0 +1,85 @@
+// Yahoo RSS url：https://news.yahoo.co.jp/rss
+
+console.log("script.js");
+/////////////////////////// 日付変換 ////////////////////////////
+function formatDate(pubDate){
+    const date = new Date(pubDate);
+    const year = date.getUTCFullYear();
+    const month = date.getUTCMonth();
+    const day = date.getUTCDate();
+    const hours = date.getUTCHours();
+    const minutes = date.getUTCMinutes();
+    const seconds = date.getUTCSeconds();
+
+    return `${year}年${month}月${day}日${hours}時${minutes}分${seconds}秒`
+}
+
+$("#btn").on("click",function(e){
+    console.log("button click");
+    e.preventDefault();
+    console.log(e);
+    $("#output").empty();
+    $("#radio_error").empty();
+
+    let selectVal = $("input[name='rss-select']:checked").val();
+    console.log(selectVal);
+    
+    let dataTosend={
+        val:selectVal,
+    }
+
+    $.ajax({
+        url:'/get_rss_xml',
+        type:"GET",
+        data:dataTosend,
+        dataType:'xml'
+        
+    })//ajax
+    .done(function(xml){
+        console.log("success");
+        console.log(xml);
+        //取得したxmlを画面に表示する
+        $(xml).find("item").each(function(){
+            //タイトルを取得する
+            let titleText = $(this).find("title").text();
+            let linkText = $(this).find("link").text();
+            let pubDate = formatDate($(this).find("pubDate").text());
+            let imgSrc = $(this).find("image").text();
+            //出力
+            $("#output").append(
+                `<div class="news">
+                <h2>${titleText}</h2>
+                <p class="pubDate">${pubDate}</p>
+                <div class="img-container">
+                    <img src="${imgSrc}"></img>
+                </div>
+                <li><a href="${linkText}" target="_blank">記事へ（別タグで表示します）</a><li>
+               
+                </div>`
+            )
+
+        })//each
+    })//done
+    .fail(function(){
+        console.log("fail");
+        $("#radio_error").append(`<p class="error_mes">ラジオボタンを押してください</p>`)
+        
+    })//fail
+});//on
+
+//////////////////////////////////////////////////////////////////scrollToTopボタン//////////////////////////////////////////////////////////////////////////
+const scrollToTop = $("#scrollToTop");
+$(window).on('scroll',function(){
+    if($(window).scrollTop()>500){
+        scrollToTop.addClass('show');
+    }
+    else{
+        scrollToTop.removeClass('show');
+    }
+});
+
+scrollToTop.on('click',function(){
+    $('html,body').animate({
+        scrollTop:0,
+    },'smooth');
+});
